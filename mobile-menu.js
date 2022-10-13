410038c5b4642b119442d0473af2f4b3647c4528
NataNow <nataszaU26@gmail.com>
2022-10-13 16:09:58 +0300

    finish

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,26 +1,19 @@
 (() => {
-  const mobileMenubtn = document.querySelector('.mobile-menu__btn');
-  const mobileMenu = document.querySelector('.mobile-menu');
-  // const closeMenuBtn = document.querySelector('.header__menu-icon--close');
-
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
   const toggleMenu = () => {
-    const isMenuOpen = mobileMenubtn.getAttribute('aria-expanded') === 'true' || false;
-    mobileMenubtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenubtn.classList.toggle('is-open');
+    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-
-    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    document.body.classList.toggle('scroll-lock');
   };
-
-  mobileMenubtn.addEventListener('click', toggleMenu);
-  // closeMenuBtn.addEventListener('click', toggleMenu);
-
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
-    mobileMenubtn.classList.remove('is-open');
-    mobileMenubtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
   });
 })();
